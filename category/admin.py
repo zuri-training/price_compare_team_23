37ff553b7932984d95f37d2048cc112525c3eedb
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-07-30 18:16:23 +0000

    product models registered

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+
+# ************** Register Models **************
+
+# + models created for the product details
+# + that the customers might see or query the system
+# + are registered here
+
+
+admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+    search_fields = ('name','id')

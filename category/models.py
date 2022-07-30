ef03bc62a109a5f2e7d2a04affe2143a6a3e618e
mac <ezefavourchiamaka01@gmail.com>
2022-07-30 21:05:44 +0100

    Added comment to models.py

--- category/models.py
+++ category/models.py
@@ -10,7 +10,7 @@ from django.urls import reverse
 # displayed to the customer with the needed
 # information on the product(s)
 
-lass Product(models.Model):
+class Product(models.Model):
     CATEGORY_CHOICES = (
         ('smartphones', 'Smartphones'),
         ('laptops','Laptops'),
@@ -42,3 +42,17 @@ lass Product(models.Model):
         ])
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='comments')
+    username = models.CharField(max_length=614)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.username} on {self.product}'

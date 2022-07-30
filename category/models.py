96163a9b4e9a5a6b191c70a955165a354c7be552
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-07-30 17:51:21 +0000

    structured the product model classes

--- category/models.py
+++ category/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.text import slugify
+from uuid import uuid4
+from django.urls import reverse
 
-# Create your models here.
+
+# ************** Product Model ***********
+
+# This model create how the product will be
+# displayed to the customer with the needed
+# information on the product(s)
+
+lass Product(models.Model):
+    CATEGORY_CHOICES = (
+        ('smartphones', 'Smartphones'),
+        ('laptops','Laptops'),
+        ('desktop-computers','Desktop-computers'),
+        ('television','Television'),
+        ('smart-tvs','Smart-tvs'),
+        ('digital-cameras','Digital-cameras'),
+        ('generators','Generators')
+    )
+    id = models.UUIDField(default=uuid4,unique=True,primary_key=True)
+    name = models.CharField(max_length=614)
+    brand = models.CharField(max_length=256)
+    img_src = models.CharField(max_length=614)
+
+
+    #price = models.CharField(max_length=600)
+    slug = models.SlugField(blank=True,max_length=124)
+    category = models.CharField(max_length=32,choices=CATEGORY_CHOICES)
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        super(Product,self).save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        #product_detail is the view
+        return reverse("price_compare:product_detail", args=[
+            self.id,
+            self.slug
+        ])
+    def __str__(self):
+        return self.name

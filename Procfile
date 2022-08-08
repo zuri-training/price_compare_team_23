66487e209d75999debfa9a9334232a027187cce2
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-08-08 12:06:42 +0000

    updated links in result.html

--- Procfile
+++
@@ -1,1 +0,0 @@
-web gunicorn price_compare_team_23.wsgi --log-file -
--- accounts/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from uuid import uuid4
-from django.urls import reverse
-
-# Create your models here.

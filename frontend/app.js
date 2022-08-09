7a2c349034d0a77b01e96dd4081599cd281348db
Slenzy2 <tobiikupolati33@gmail.com>
2022-08-10 00:42:13 +0100

    Complete & updated product page

--- frontend/app.js
+++ frontend/app.js
@@ -1,10 +1,10 @@
 // Hamburger Setup
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('is-active');
-    menu.classList.toggle('is-active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  menu.classList.toggle("is-active");
 });
 
 // Product Page
@@ -34,105 +34,117 @@ prevButton.addEventListener("click", () => {
 // Images
 const cats = [
   {
-    title: "Popular Products",
+    title: "Popular Categories",
     products: [
       {
-        name: "",
-        image: "zuri.png",
+        name: "Iphones",
+        image: "phone_category.png",
       },
       {
-        name: "",
-        image: "second.png",
+        name: "Cameras",
+        image: "camera_category.png",
       },
       {
-        name: "",
-        image: "third.png",
+        name: "Mobile Phones",
+        image: "androids_category.png",
       },
       {
-        name: "",
-        image: "fourth.png",
+        name: " Laptops",
+        image: "laptops_category.png",
       },
       {
-        name: "",
-        image: "fifth.png",
+        name: "Tablets",
+        image: "tablets_category.png",
       },
     ],
   },
-  
+
   {
-    title: "Mobile devices",
+    title: "Mobile Phones",
     products: [
       {
-        name: "",
-        image: "zuri.png",
+        name: "Iphone 13",
+        image: "phone1_product.png",
       },
       {
-        name: "",
-        image: "second.png",
+        name: "Galaxy Note 10",
+        image: "phone2_product.png",
       },
       {
-        name: "",
-        image: "third.png",
+        name: "Infinix Note 12",
+        image: "phone3_product.png",
       },
       {
-        name: "",
-        image: "fourth.png",
+        name: "Iphone Xs max",
+        image: "phone4_product.png",
       },
       {
-        name: "",
-        image: "fifth.png",
+        name: "Infinix ZeroX",
+        image: "phone5_product.png",
+      },
+      {
+        name: "Y9-2019",
+        image: "phone6_product.png",
       },
     ],
   },
-  
+
   {
-    title: "Home electronics",
+    title: "Cameras",
     products: [
       {
-        name: "",
-        image: "zuri.png",
+        name: "Canon EOS-5d",
+        image: "camera1_product.png",
       },
       {
-        name: "",
-        image: "second.png",
+        name: "Nikon L340",
+        image: "camera2_product.png",
       },
       {
-        name: "",
-        image: "third.png",
+        name: "Nikon D3100",
+        image: "camera3_product.png",
       },
       {
-        name: "",
-        image: "fourth.png",
+        name: "Canon 550d",
+        image: "camera4_product.png",
       },
       {
-        name: "",
-        image: "fifth.png",
+        name: "Sony Dsc-Hx60",
+        image: "camera5_product.png",
+      },
+      {
+        name: "Nikon Z-6",
+        image: "camera6_product.png",
       },
     ],
   },
-  
+
   {
     title: "Desktop and laptops",
     products: [
       {
-        name: "",
-        image: "zuri.png",
+        name: "Lenovo core i3",
+        image: "laptop1_product.png",
       },
       {
-        name: "",
-        image: "second.png",
+        name: "Intel Core  i7 ",
+        image: "laptop2_product.png",
       },
       {
-        name: "",
-        image: "third.png",
+        name: "HP core i7",
+        image: "laptop3_product.png",
       },
       {
-        name: "",
-        image: "fourth.png",
+        name: "Dell Inspiron",
+        image: "laptop4_product.png",
       },
       {
-        name: "",
-        image: "fifth.png",
+        name: "Lenovo Idea pad",
+        image: "laptop5_product.png",
+      },
+      {
+        name: "Siemens Simatic",
+        image: "laptop6_product.png",
       },
     ],
   },
@@ -145,6 +157,7 @@ cats.forEach((item) => {
   item.products.forEach((product) => {
     productsInCategory += `<div class="single-product">
                 <img src="img/${product.image}" alt="" />
+                <p class="product_name"> ${product.name} </p>
             </div>`;
   });
 
@@ -158,13 +171,14 @@ categories.innerHTML = singleCategory;
 
 // Toggle
 
-const togglePassword = document.querySelector('#togglePassword');
-  const password = document.querySelector('#password');
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
 
-  togglePassword.addEventListener('click', function () {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the icon
-    this.classList.toggle('bi-eye');
+togglePassword.addEventListener("click", function () {
+  // toggle the type attribute
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  // toggle the icon
+  this.classList.toggle("bi-eye");
 });
